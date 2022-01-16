f6cb169b864131587c7694b4e7d8b106498f475a
Lawrence Great <greatlaw2@gmail.com>
2022-01-16 04:06:37 +0400

    Update Single Table Analysis Part One.sql
    remove part 2 code

--- Single Table Analysis Part One.sql
+++ Single Table Analysis Part One.sql
@@ -73,15 +73,3 @@ SELECT title, length, rental_rate
 FROM film
 ORDER BY length DESC;
 
--- SINGLE TABLE ANALYSIS(Part 2): Assignment (The CASE Statement)
-
-SELECT first_name,
-       last_name,
-       CASE
-           WHEN store_id = 1 AND active = 1 THEN 'store 1 active'
-           WHEN store_id = 1 AND active = 0 THEN 'store 1 inactive'
-           WHEN store_id = 2 AND active = 1 THEN 'store 2 active'
-           WHEN store_id = 2 AND active = 0 THEN 'store 2 inactive'
-           ELSE 'check logic'
-           END AS store_and_status
-FROM customer;
